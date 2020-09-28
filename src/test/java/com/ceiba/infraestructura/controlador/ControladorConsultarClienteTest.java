3f5f9492e2c6519f3e449fe32e6aad56e5c3617a
Giovanny Gómez - Ceiba Software <giovanny.gomez@ceiba.com.co>
2020-09-28 17:06:21 -0500

    fix: application test

--- src/test/java/com/ceiba/infraestructura/controlador/ControladorConsultarClienteTest.java
+++ src/test/java/com/ceiba/infraestructura/controlador/ControladorConsultarClienteTest.java
@@ -1,11 +1,15 @@
 package com.ceiba.infraestructura.controlador;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,7 +25,7 @@ public class ControladorConsultarClienteTest {
     private MockMvc mocMvc;
 
     @Test
-    public void listarClientes() throws Exception {
+    public void listarClientes(@Autowired MongoTemplate mongoTemplate) throws Exception {
         mocMvc.perform(MockMvcRequestBuilders
                 .get("/correrias/clientes")
                 .contentType(MediaType.APPLICATION_JSON))
